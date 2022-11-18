0c06e9a08fae0ee82c2e982d58e2ac8431b98eb4
Domara Hawil <domara.hawil@hotmail.com>
2022-11-18 14:58:42 -0500

    pairprogramming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -24,15 +24,16 @@ describe("#wordSearch()", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['Y', 'M', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'I', 'I', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'MIKE')
 
     assert.isTrue(result);
   });
 });
+
